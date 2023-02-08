f41ebe882f75b2685625cd633eb6203001aa9e8d
Youngben1 <benjaminrenny@gmail.com>
2023-02-08 21:59:02 +0100

    customers and orders section

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import data from "../data/data.js";
-import { FashoppingBag } from "react-icons/fa";
+import { data } from "../data/data.js";
+import { FaShoppingBag } from "react-icons/fa";
 
 const RecentOrders = () => {
   return (
     <div className="w-full col-span-1relative lg:h-[70vh] h-[50vh] p-4 border rounded-lg bg-white overflow-scroll">
       <h1>Recent Orders</h1>
+      <ul>
+        {data?.map((order, id) => (
+          <li
+            key={id}
+            className="flex items-center p-2 my-3 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+          >
+            <div className="p-3 bg-purple-100 rounded-lg">
+              <FaShoppingBag className="text-purple-800" />
+            </div>
+            <div className="pl-4">
+              <p className="font-bold text-gray-800">${order.total}</p>
+              <p className="text-sm text-gray-400">{order.name.first}</p>
+            </div>
+            <p className="absolute text-sm lg:flex md:hidden right-6">
+              {order.date}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
