0ff2c6f535d0fc6c70eddfe24c07d76dbd848412
Youngben1 <benjaminrenny@gmail.com>
2023-02-17 14:12:33 +0100

    orders section

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -1,7 +1,52 @@
+import { BsPersonFill, BsThreeDotsVertical } from "react-icons/bs";
 import React from "react";
+import { data } from "../data/data";
 
 const customers = () => {
-  return <div>customers</div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex justify-between p-4">
+        <h2>Customers</h2>
+        <h2>Welcome Back, Ben.</h2>
+      </div>
+
+      <div className="p-4">
+        <div className="w-full p-4 m-auto overflow-y-auto bg-white border rounded-lg">
+          <div className="grid items-center justify-between grid-cols-2 p-2 my-3 cursor-pointer sm:grid-cols-3 md:grid-cols-4 ">
+            <span>Name</span>
+            <span className="text-right sm:text-left">Email</span>
+            <span className="hidden md:grid">Last Order</span>
+            <span className="hidden sm:grid">Method</span>
+          </div>
+          <ul>
+            {data?.map((order, id) => (
+              <li
+                key={id}
+                className="grid items-center justify-between grid-cols-2 p-2 my-3 rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 md:grid-cols-4 sm:grid-cols-3"
+              >
+                <div className="flex items-center">
+                  <div className="bg-purple-100 rounded-lg">
+                    <BsPersonFill className="text-purple-800" />
+                  </div>
+                  <p className="pl-4">
+                    {order.name.first + " " + order.name.last}
+                  </p>
+                  <p className="text-right text-gray-600 sm:text-left">
+                    {order.name.first}@gmail.com
+                  </p>
+                  <p className="hidden md:flex">{order.date}</p>
+                  <div className="items-center justify-between hidden sm:flex">
+                    <p>{order.method}</p>
+                    <BsThreeDotsVertical className="" />
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default customers;
